270e8bad2c3a23a0ee2f53c9d459c07eea8075a4
Sue <127782189+SsuessV@users.noreply.github.com>
2026-05-26 20:56:01 +0200

    Add files via upload

---
+++ ft_substr (1).c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: syoun <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/25 20:11:05 by syoun             #+#    #+#             */
+/*   Updated: 2023/09/25 20:11:16 by syoun            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t			i;
+	size_t			s_len;
+	char			*substr;
+
+	if (!s)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		return (ft_calloc(1, 1));
+	if (len > s_len - start)
+		len = s_len - start;
+	substr = malloc((len + 1) * sizeof(char));
+	if (!substr)
+		return (NULL);
+	i = 0;
+	while (i < len && s[start + i] != '\0')
+	{
+		substr[i] = s[start + i];
+		i++;
+	}
+	substr[i] = '\0';
+	return (substr);
+}
+/*
+int main(void)
+{
+    char s[] = "Hihello Julian Uitz";
+    printf("%s\n", ft_substr(s, 2, 5));
+    return (0);
+}
+*/
